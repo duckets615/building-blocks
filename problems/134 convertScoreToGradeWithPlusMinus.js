625a422f37287a53ad80ba741ac80bacfc338d37
duckets615 <chris.reilly024@gmail.com>
2019-06-08 21:31:41 -0400

    new 132, 134, 140

---
+++ problems/134 convertScoreToGradeWithPlusMinus.js
@@ -0,0 +1,31 @@
+//  134  Convert Score to grade with plus minus
+
+
+/*
+Write a function called "convertScoreToGradeWithPlusAndMinus".
+
+Given a score, "convertScoreToGradeWithPlusAndMinus" returns a string representing the letter grade corresponding to the given score.
+
+Notes:
+* (100 - 90) --> 'A'
+* (89  - 80) --> 'B'
+* (79  - 70) --> 'C'
+* (69  - 60) --> 'D'
+* (60  -  0) --> 'F'
+* If the given score is greater than 100 or less than 0, it should return 'INVALID SCORE'.
+* If the score is between the 0 and 2 (inclusive) of a given range, return the letter with a '-'
+* If the score is be the 8 and 9 (inclusive) of a given range, return the letter with a '+'
+* There are is no F+ and there is no F-.
+
+var output = convertScoreToGradeWithPlusAndMinus(91);
+console.log(output); // --> 'A-'
+*/
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+    // code here
+}
+
+console.log(convertScoreToGradeWithPlusAndMinus(95)) // 'A'
+console.log(convertScoreToGradeWithPlusAndMinus(77)) // 'C'
+console.log(convertScoreToGradeWithPlusAndMinus(58)) // 'F'
+  
---
+++ solutions/134 convertScoreToGradeWithPlusMinus.js
@@ -0,0 +1,46 @@
+//  134  Convert Score to grade with plus minus
+
+
+/*
+Write a function called "convertScoreToGradeWithPlusAndMinus".
+
+Given a score, "convertScoreToGradeWithPlusAndMinus" returns a string representing the letter grade corresponding to the given score.
+
+Notes:
+* (100 - 90) --> 'A'
+* (89  - 80) --> 'B'
+* (79  - 70) --> 'C'
+* (69  - 60) --> 'D'
+* (60  -  0) --> 'F'
+* If the given score is greater than 100 or less than 0, it should return 'INVALID SCORE'.
+* If the score is between the 0 and 2 (inclusive) of a given range, return the letter with a '-'
+* If the score is be the 8 and 9 (inclusive) of a given range, return the letter with a '+'
+* There are is no F+ and there is no F-.
+
+var output = convertScoreToGradeWithPlusAndMinus(91);
+console.log(output); // --> 'A-'
+*/
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+    if (score > 100 || score < 0) return 'Invalid Score'  
+    if (score >= 98) return 'A+'
+    if (score >= 93) return 'A'
+    if (score >= 90) return 'A-'
+    if (score >= 88) return 'B+'
+    if (score >= 83) return 'B'
+    if (score >= 80) return 'B-'
+    if (score >= 78) return 'C+'
+    if (score >= 73) return 'C'
+    if (score >= 70) return 'C-'
+    if (score >= 68) return 'D+'
+    if (score >= 63) return 'D'
+    if (score >= 60) return 'D-'
+    if (score <= 59) return 'F'  
+}
+
+console.log(convertScoreToGradeWithPlusAndMinus(99)) // 'A+'
+console.log(convertScoreToGradeWithPlusAndMinus(72)) // 'C-'
+console.log(convertScoreToGradeWithPlusAndMinus(85)) // 'B'
+console.log(convertScoreToGradeWithPlusAndMinus(58)) // 'F'
+console.log(convertScoreToGradeWithPlusAndMinus(101)) // 'Invalid Score'
+  
